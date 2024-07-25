eb3e6d73c6f0e379242be0ede6907f32763de4e9
srinathren <113780546+srinathren@users.noreply.github.com>
2024-07-25 12:52:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
-import { VRButton } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/webxr/VRButton.js";
+import * as THREE from "https://cdn.skypack.dev/three@0.166.1/build/three.module.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.166.1/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.166.1/examples/jsm/loaders/GLTFLoader.js";
+import { VRButton } from "https://cdn.skypack.dev/three@0.166.1/examples/jsm/webxr/VRButton.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
